094dc408bcc7fb79dc2f3fa7bd395171b1be5ccb
Hayk <h.hambardzumyan21@gmail.com>
2022-12-02 15:56:49 +0400

    NEW

--- src/main/java/Page.java
+++ src/main/java/Page.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class Page {
-}
+public class Page {
+
+        private PageInfo pageInfo;
+        private Post[] posts;
+
+        public PageInfo getPageInfo() {
+            return pageInfo;
+        }
+
+        public void setPageInfo(PageInfo pageInfo) {
+            this.pageInfo = pageInfo;
+        }
+
+        public Post[] getPosts() {
+            return posts;
+        }
+
+        public void setPosts(Post[] posts) {
+            this.posts = posts;
+        }
+
+    }
+
--- src/main/java/PageInfo.java
+++ src/main/java/PageInfo.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class PageInfo {
+public class PageInfo {
+
+    private String pageName;
+    private String pagePic;
+
+    public String getPageName() {
+        return pageName;
+    }
+
+    public void setPageName(String pageName) {
+        this.pageName = pageName;
+    }
+
+    public String getPagePic() {
+        return pagePic;
+    }
+
+    public void setPagePic(String pagePic) {
+        this.pagePic = pagePic;
+    }
+
 }
